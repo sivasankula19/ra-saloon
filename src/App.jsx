8be5205935ca2a96996d86ecc638d5a9191c0ef0
KrinSys - Siva #1 <sankulashiva09@gmail.com>
2026-04-05 14:08:29 +0530

    Initiate Repo with basic setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import RASaloon from "./RaSaloon";
+
+function App() {
+  return (
+    <>
+      <RASaloon />
+    </>
+  );
+}
+
+export default App;
